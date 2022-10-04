7bd8513fa0053b2e63dbfa017176de7580ee8f62
Fergus-MW <gangevo@gmail.com>
2022-10-04 10:11:04 +0100

    fixed angle outputs

--- Control.py
+++ Control.py
@@ -21,21 +21,23 @@ matplotlib.pyplot.show()
 
 def sendAngles(angles):
     arm = xarm.Controller('USB')
-    print('Battery voltage in volts:', arm.getBatteryVoltage())
-    arm.setPosition(1, angles[5])
+    arm.setPosition(6, angles[1])
     time.sleep(0.01)
-    arm.setPosition(2, angles[4])
+    arm.setPosition(2, angles[5])
     time.sleep(0.01)
-    arm.setPosition(3, angles[3])
+    arm.setPosition(3, -angles[4])
     time.sleep(0.01)
-    arm.setPosition(4, angles[2])
+    arm.setPosition(4, angles[3])
     time.sleep(0.01)
-    arm.setPosition(5, angles[1])
-    time.sleep(0.01)
-    arm.setPosition(6, angles[0])
+    arm.setPosition(5, angles[2])
 
 def calcAngles(x,y,z):
     res = my_chain.inverse_kinematics([x,y,z])
+    
+    matplotlib.pyplot.clf()
+    ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
+    my_chain.plot(my_chain.inverse_kinematics([x, y, z]), ax)
+    matplotlib.pyplot.show()
     print(res)
     res = [math.degrees(item) for item in res]
     return res
@@ -44,7 +46,11 @@ def controlArm(x,y,z):
     angles = calcAngles(x,y,z)
     print (angles)
     sendAngles(angles)
-    matplotlib.pyplot.clf()
-    ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
-    my_chain.plot(my_chain.inverse_kinematics([x, y, z]), ax)
-    matplotlib.pyplot.show()
+    
+def grab():
+    arm = xarm.Controller('USB')
+    arm.setPosition(1,1000)
+
+def release():
+    arm = xarm.Controller('USB')
+    arm.setPosition(1,0)
